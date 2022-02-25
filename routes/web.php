86fafe5b49c66b314352e1117872a7cca61e4b05
abrahamkivosh <abrahamkivosh@gmail.com>
2022-02-25 21:07:05 +0300

    changes route route

--- routes/web.php
+++ routes/web.php
@@ -14,16 +14,16 @@ use App\Http\Controllers\PageController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Auth::routes();
-Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 
 // client pages
-Route::get('/ind',[PageController::class,'index'])->name('index');
+Route::get('/',[PageController::class,'index'])->name('index');
 Route::get('/about',[PageController::class,'about'])->name('about');
 Route::get('/contact',[PageController::class,'contact'])->name('contact');
 Route::get('/blog',[PageController::class,'blog'])->name('blog');
