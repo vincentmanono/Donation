384805f6b0353804a5fea74a78e20bcc9fc15396
vincentmanono <you@example.comvincentnyamongo95@gmail.com>
2022-02-28 14:18:45 +0300

    working on donor crud..

---
+++ resources/views/admin/donors/index.blade.php
@@ -0,0 +1,82 @@
+@extends('layouts.adminapp')
+@section('content')
+<div class="container-fluid">
+    <!-- ============================================================== -->
+    <!-- Bread crumb and right sidebar toggle -->
+    <!-- ============================================================== -->
+    <div class="row page-titles">
+        <div class="col-md-5 align-self-center">
+            <h4 class="text-themecolor">Donors Table</h4>
+        </div>
+        <div class="col-md-7 align-self-center text-right">
+            <div class="d-flex justify-content-end align-items-center">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="{{route('home')}}">Home</a></li>
+                    <li class="breadcrumb-item active">Donors</li>
+                </ol>
+                <a href="{{route('register')}}" class="btn btn-info d-none d-lg-block m-l-15"><i
+                        class="fa fa-plus-circle"></i> Create New</a>
+
+            </div>
+        </div>
+    </div>
+    <!-- ============================================================== -->
+    <!-- End Bread crumb and right sidebar toggle -->
+    <!-- ============================================================== -->
+    <!-- ============================================================== -->
+    <!-- Start Page Content -->
+    <!-- ============================================================== -->
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body">
+                    <h4 class="card-title">Donors </h4>
+                    <div class="table-responsive m-t-40">
+                        <table id="example23" class="display nowrap table table-hover table-striped table-bordered"
+                            cellspacing="0" width="100%">
+                            <thead>
+                                <tr>
+                                    <th>Doneted By</th>
+                                    <th>Email</th>
+                                    <th>Action</th>
+                                </tr>
+
+                            </thead>
+                            <tfoot>
+                                <tr>
+                                    <th>Doneted By</th>
+                                    <th>Email</th>
+                                    <th>Action</th>
+                                </tr>
+                            </tfoot>
+                            <tbody>
+                                @foreach ($donors as $donor)
+                                <tr>
+                                    <td> {{$donor->name}} </td>
+                                    <td> {{ $donor->email}} </td>
+                                    <td>
+                                        <div class="row">
+                                            <div>
+                                            <a href="{{route('donors.show',$donor)}}"> <i class="fa fa-eye" aria-hidden="true">View</i> </a>
+                                                     </td>
+                                        </div>
+                                        </div>
+
+
+                                    </td>
+
+                                </tr>
+
+                                @endforeach
+
+
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
